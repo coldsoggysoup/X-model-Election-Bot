e912bf547242f33c49c2cec83491df41589c7832
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2026-05-19 16:50:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+	print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+	if message.author == client.user:
+		return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+	if message.content.startswith('$hello'):
+		await message.channel.send('Hello!')
 	
 	if message.content.startswith('$ehco'):
 		await message.channel.send(message.content[6:])
